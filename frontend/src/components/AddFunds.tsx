117c685d78813bc03c0103a371f6b41c0af281ca
Adam Brandt <57259688+adkadt@users.noreply.github.com>
2025-11-17 17:51:38 -0500

    added add funds section

---
+++ frontend/src/components/AddFunds.tsx
@@ -0,0 +1,58 @@
+// AddFunds.tsx
+// import { useEffect } from 'react';
+import './AddFunds.css';
+import { buildPath } from './Path';
+
+interface ModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+const Modal: React.FC<ModalProps> = ({ isOpen, onClose }) => {
+    if (!isOpen) return null;
+    
+    const addFunds = (num: number) => {
+        const jwtToken = sessionStorage.getItem("jwtToken");
+        
+        if (jwtToken) {
+            try {
+            fetch(buildPath('api/addcredits'), {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ credits: num, jwtToken }),
+            });
+            // fetchBalance()
+            } catch (err) {
+            console.error(err);
+            }
+        }
+    }
+
+    return (
+        <div id="addfunds-overlay" onClick={onClose}>
+            <div id="addfunds-content" onClick={(e) => e.stopPropagation()}>
+                <div id="addfunds-header">
+                    <h2>Add Funds</h2>
+                <button id="addfunds-close" onClick={onClose}>
+                    &times;
+                </button>
+                </div>
+                <div id="addfunds-body">
+                    <div id="addfunds-selecter">
+                        {[1, 5, 25, 100, 500, 1000].map(value => (
+                            <button
+                            key={value}
+                            className={`addfunds-button`}
+                            onClick={() => addFunds(value)}
+                            >
+                                ¤{value}
+                            </button>
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Modal;
--- frontend/src/components/AppHeader.tsx
+++ frontend/src/components/AppHeader.tsx
@@ -1,10 +1,13 @@
 import "./AppHeader.css"
-
+import { useState } from "react";
 import ActiveUsername from "./ActiveUsername";
 import {useLocation, Link } from "react-router-dom";
+import AddFunds from "./AddFunds";
 
 function AppHeader()
 {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const location = useLocation()
 
     function doLogout(event:any) : void
     {
@@ -14,36 +17,54 @@ function AppHeader()
         window.location.href = '/';
     };    
 
-    const location = useLocation()
 
     return (
-        <div id="app-header">
-            <header id="roulette-header">
-                <ActiveUsername />
-                <div id="header-actions">
-                    
-                    {location.pathname !== '/leaderboard' && (
-                        <div className="header-button-div">
-                            <Link to="/leaderboard">
-                                <input type="button" className="header-button" value="Leaderboard" />
-                            </Link>
-                        </div>
-                    )}
+        <>
+            <div id="app-header">
+                <header id="roulette-header">
+                    <ActiveUsername />
+                    <div id="header-actions">
 
-                    {location.pathname !== '/roulette' && (
-                        <div className="header-button-div">
-                            <Link to="/roulette">
-                                <input type="button" className="header-button" value="Roulette" />
-                            </Link>
-                        </div>
-                    )}
+                        {location.pathname === '/roulette' && (
+                            <div className="header-button-div">
+                                <input 
+                                    type="button" 
+                                    className="header-button" 
+                                    value="Add Funds" 
+                                    onClick={() => setIsModalOpen(true)} 
+                                    />
+                            </div>
+                        )}
+                        
+                        {location.pathname !== '/leaderboard' && (
+                            <div className="header-button-div">
+                                <Link to="/leaderboard">
+                                    <input type="button" className="header-button" value="Leaderboard" />
+                                </Link>
+                            </div>
+                        )}
+                        
+                        {location.pathname !== '/roulette' && (
+                            <div className="header-button-div">
+                                <Link to="/roulette">
+                                    <input type="button" className="header-button" value="Roulette" />
+                                </Link>
+                            </div>
+                        )}
 
-                    <div className="header-button-div">
-                        <input type="submit" className="header-button" value = "Log Out" onClick={doLogout} /><br />
+                        <div className="header-button-div">
+                            <input type="submit" className="header-button" value = "Log Out" onClick={doLogout} /><br />
+                        </div>
                     </div>
-                </div>
-            </header>
-        </div>
+                </header>
+            </div>
+
+            <AddFunds 
+                isOpen={isModalOpen} 
+                onClose={() => setIsModalOpen(false)}
+            />
+        </>
+
     )
 }
 
