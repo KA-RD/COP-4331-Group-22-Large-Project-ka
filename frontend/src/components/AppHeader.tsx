38bd6bbfbbeb56d9a2d23566b8a565f6d276766b
Adam Brandt <57259688+adkadt@users.noreply.github.com>
2025-11-17 16:55:44 -0500

    changed app header to have return from leaderboard

--- frontend/src/components/AppHeader.tsx
+++ frontend/src/components/AppHeader.tsx
@@ -1,23 +1,46 @@
 import "./AppHeader.css"
 
 import ActiveUsername from "./ActiveUsername";
-import Logout from "./Logout"
-import { Link } from "react-router-dom";
-
+import {useLocation, Link } from "react-router-dom";
 
 function AppHeader()
 {
+
+    function doLogout(event:any) : void
+    {
+        event.preventDefault();
+        
+        localStorage.removeItem("user_data")
+        window.location.href = '/';
+    };    
+
+    const location = useLocation()
+
     return (
         <div id="app-header">
             <header id="roulette-header">
                 <ActiveUsername />
                 <div id="header-actions">
-                  <div id="leaderboard-div">
-                    <Link to="/leaderboard">
-                      <input type="button" id="leaderboard-button" value="Leaderboard" />
-                    </Link>
-                  </div>
-                  <Logout />
+                    
+                    {location.pathname !== '/leaderboard' && (
+                        <div className="header-button-div">
+                            <Link to="/leaderboard">
+                                <input type="button" className="header-button" value="Leaderboard" />
+                            </Link>
+                        </div>
+                    )}
+
+                    {location.pathname !== '/roulette' && (
+                        <div className="header-button-div">
+                            <Link to="/roulette">
+                                <input type="button" className="header-button" value="Roulette" />
+                            </Link>
+                        </div>
+                    )}
+
+                    <div className="header-button-div">
+                        <input type="submit" className="header-button" value = "Log Out" onClick={doLogout} /><br />
+                    </div>
                 </div>
             </header>
         </div>
